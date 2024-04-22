7e2eeef7b773640605eb0b811b64e6067ba17a9a
Jonas Pamminger <j.pamminger@HTBLALeonding.onmicrosoft.com>
2024-04-22 14:55:59 +0200

    implement emgu cv

--- Backend/Controllers/StreamingController.cs
+++ Backend/Controllers/StreamingController.cs
@@ -20,20 +20,15 @@ public class StreamingController : ControllerBase
         // Process the frame using Emgu CV
         using (var image = new Mat())
         {
-            // CvInvoke.Imdecode(frameBytes, ImreadModes.Color, image);
-            // if (image.IsEmpty)
-            // {
-            //     return new HttpResponseMessage(HttpStatusCode.BadRequest);
-            // }
+            CvInvoke.Imdecode(frameBytes, ImreadModes.Color, image);
+            if (image.IsEmpty)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
 
-            // Face detection logic with Emgu CV
-            // string assemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
-            // string assemblyPath = Path.GetDirectoryName(assemblyLocation);
-            // string path = Path.Combine(assemblyPath, "Resources", "haarcascade_frontalface_default.xml");
-
-            // var faceDetector =
-            //     new CascadeClassifier(path); // Replace with your face detection model path
-            // Rectangle[] faces = faceDetector.DetectMultiScale(image, 1.1, 3, Size.Empty, Size.Empty);
+            var faceDetector =
+                new CascadeClassifier(@".\Resources\haarcascade_frontalface_default.xml"); // Replace with your face detection model path
+            Rectangle[] faces = faceDetector.DetectMultiScale(image, 1.1, 3, Size.Empty, Size.Empty);
 
             // Logic to handle detected faces (e.g., send response)
             // ...
