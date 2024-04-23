fe012e405869644dc844d96ba6d40e231921ce0a
Paul Nell <57170160+paul1610@users.noreply.github.com>
2024-04-23 07:28:38 +0200

    code cleanup

--- Backend/Controllers/StreamingController.cs
+++ Backend/Controllers/StreamingController.cs
@@ -13,14 +13,14 @@ public class StreamingController : ControllerBase
 {
     public class FrameObject
     {
-        public string Data { get; set; }
+        public string Data { get; set; } = "";
     }
 
     [HttpPost]
-    public async Task<ActionResult<string>> SendFrame([FromBody] FrameObject frame)
+    public ActionResult<string> SendFrame([FromBody] FrameObject frame)
     {
         // Decode the frame data from base64
-        byte[] frameBytes = Convert.FromBase64String(frame.Data.Split(",")[1]);
+        var frameBytes = Convert.FromBase64String(frame.Data.Split(",")[1]);
 
         // Process the frame using Emgu CV
         using (var image = new Mat())
@@ -31,7 +31,7 @@ public class StreamingController : ControllerBase
 
             var faceDetector =
                 new CascadeClassifier(@".\Resources\haarcascade_frontalface_default.xml");
-            Rectangle[] faces = faceDetector.DetectMultiScale(image, 1.1, 3, Size.Empty, Size.Empty);
+            var faces = faceDetector.DetectMultiScale(image, 1.1, 3, Size.Empty, Size.Empty);
 
             return Ok(faces);
         }
