d1befc4ef7094dc655a61bdc23b8ff79854e5082
Leon_Leeb <you.t5be@gmx.at>
2024-11-30 22:00:53 +0100

    Create BackgroundImageController.cs

---
+++ Backend/Controllers/BackgroundImageController.cs
@@ -0,0 +1,44 @@
+﻿using backend.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Authorize]
+public class BackgroundImageController(DataContext ctx) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<BackgroundImage>>> GetBackgroundImages()
+    {
+        return await ctx.BackgroundImages.ToListAsync();
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<BackgroundImage>> AddBackgroundImage(CreateBackgroundImage bi)
+    {
+        var backgroundImage = new BackgroundImage
+        {
+            Base64Image = bi.Base64Image
+        };
+        
+        ctx.BackgroundImages.Add(backgroundImage);
+        await ctx.SaveChangesAsync();
+
+        return CreatedAtAction("GetBackgroundImage", new { id = backgroundImage.Id }, backgroundImage);
+    }
+    
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> DeleteBackgroundImage(int id)
+    {
+        var bi = await ctx.BackgroundImages.FindAsync(id);
+        if (bi == null) return NotFound("BackgroundImage id not found!");
+
+        ctx.BackgroundImages.Remove(bi);
+        await ctx.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
